3d9e7faed6845267c31830ff704a0c1c8a48cd39
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-02-13 21:35:51 -0300

    users routes CRUD

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../controllers/users.controllers';
+
+
+const router = Router();
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+
+export default router;
